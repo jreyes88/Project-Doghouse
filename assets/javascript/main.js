12c8ee460f509e28f05fd702b0a9f55fd9bb36ba
Michael <Multishifties.multishifties@yahoo.com>
2016-06-08 14:21:37 -0500

    Initial site setup

---
+++ assets/javascript/main.js
@@ -0,0 +1,15 @@
+$("document").ready(function(){
+	console.log("JS and jQuery is linked and working");// Confirm JS
+
+	/***  GLOBAL VARIABLES  ***/
+		// Declare vars here
+
+	/***  FUNCTIONS  ***/
+		// Build our functions here
+
+	/***  EVENTS  ***/
+		// Bind our functions to events
+
+
+
+});// End document.ready
